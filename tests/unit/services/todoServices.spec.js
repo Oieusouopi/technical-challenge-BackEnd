e53a783b869e1a68b97ab863d3fbe7d4dc713124
Oieusouopi <rafasc866@gmail.com>
2022-06-23 20:36:55 -0300

    modified: melhorando a execução do test

--- tests/unit/services/todoServices.spec.js
+++ tests/unit/services/todoServices.spec.js
@@ -4,12 +4,20 @@ const sinon = require('sinon');
 
 const allTaskMock = require('../helper/allTaskMock');
 
-const todoModels = require('../../../src/models');
-const todoServices = require('../../../src/');
+const todoModels = require('../../../src/models/todoModels');
+const todoServices = require('../../../src/services/todoServices');
 
 describe('todoServices()', () => {
+  beforeEach(() => {
+    sinon.stub(todoModels, 'getAllTaskList').resolves(allTaskMock[0]);
+  });
+
+  afterEach(() => {
+    todoModels.getAllTaskList.restore();
+  });
+
   it('Get all tasks list correctly', async () => {
-    const allTaskList = todoServices.getAllTasks();
+    const allTaskList = await todoServices.getAllTaskList();
     expect(allTaskList).to.be.equal(...allTaskMock);
   });
 });
