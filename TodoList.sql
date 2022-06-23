10788dae19466d84187d67daaa6e16955ffeafab
Oieusouopi <rafasc866@gmail.com>
2022-06-23 15:55:32 -0300

    add: DATABASE TodoList and INSERT values

---
+++ TodoList.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS TodoList;
+
+CREATE DATABASE TodoList;
+
+USE TodoList;
+
+CREATE TABLE tasks (
+    id INT NOT NULL auto_increment,
+    status VARCHAR(30) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    description VARCHAR(30),
+    PRIMARY KEY(id)
+)
+
+INSERT INTO TodoList.tasks (status, created_at, description) VALUES
+    ('pendente', NOW(), 'realizar manutenção na maquina'),
+    ('andamaneto', NOW(), 'desentupir os canos'),
+    ('pronto', NOW(), 'atender os clientes de empresa x');
