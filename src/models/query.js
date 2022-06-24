e1cd5bb7e2968cdab87b1d41d9f2afcd2624731b
Oieusouopi <rafasc866@gmail.com>
2022-06-24 13:06:28 -0300

    adicionando: adicionando query para inserir um valor na lista tasks

--- src/models/query.js
+++ src/models/query.js
@@ -1,6 +1,8 @@
 const query = {
   queryGetAllTasks: `SELECT title, status, created_at, description
     FROM TodoList.tasks ORDER BY title, created_at, status, description`,
+  queryCreateTask: `INSERT INTO TodoList.tasks
+    (title, status, created_at, description) VALUES (?, ?, ?, ?)`,
 };
 
 module.exports = query;
