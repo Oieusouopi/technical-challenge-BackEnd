8544f91b74ddccbea5d112c014ca32e501f17813
Oieusouopi <rafasc866@gmail.com>
2022-06-24 13:06:53 -0300

    adicionando: adicionando função de criar listas

--- src/models/taskListModels.js
+++ src/models/taskListModels.js
@@ -1,11 +1,19 @@
 const connection = require('./connection');
-const {queryGetAllTasks} = require('./query');
+const {queryGetAllTasks, queryCreateTask} = require('./query');
 
 const getAllTaskList = async () => {
   const [allTaskList] = await connection.execute(queryGetAllTasks);
   return allTaskList;
 };
 
+const createTask = async (title, status, date, description) => {
+  const newTask = await connection.execute(queryCreateTask,
+      [title, status, date, description]);
+  console.log(createTask);
+  return newTask;
+};
+
 module.exports = {
   getAllTaskList,
+  createTask,
 };
