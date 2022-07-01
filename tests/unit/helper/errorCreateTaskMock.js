346b621939bb4af061a5cafd6fde735c820a59ee
Oieusouopi <rafasc866@gmail.com>
2022-06-30 22:56:44 -0300

    tests: modified createTaskMock and create errorCreateTaskMock

---
+++ tests/unit/helper/errorCreateTaskMock.js
@@ -0,0 +1,10 @@
+const errorCreateTaskMock = [[
+  {
+    newId: 1,
+    TITLEERRORSIZE: 'lorem ipsum lorem ipsum lorem',
+    status: 'pronto',
+    description: 'atender os clientes de empresa x',
+  },
+]];
+
+export default errorCreateTaskMock;
