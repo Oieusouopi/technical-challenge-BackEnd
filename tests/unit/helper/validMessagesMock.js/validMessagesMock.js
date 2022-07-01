8539c830ec4680ee526b45a94f5cd1509e31940f
Oieusouopi <rafasc866@gmail.com>
2022-07-01 14:51:27 -0300

    tests: valid messages mock

---
+++ tests/unit/helper/validMessagesMock.js/validMessagesMock.js
@@ -0,0 +1,7 @@
+const validMessagesMock = {
+  TITLEMOCKSIZE: 'Wrong "title" size must be less than 30 characters',
+  STATUSERRORMOCK: `Wrong "status" should be Pendente,
+     Error, Sucesso or Em andamento`,
+};
+
+module.exports = validMessagesMock;
