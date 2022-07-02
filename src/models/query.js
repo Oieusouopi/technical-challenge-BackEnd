6cd59823e4cdb6af1d2e30d9cd1f58f825455fde
Oieusouopi <rafasc866@gmail.com>
2022-07-01 23:12:54 -0300

    modified: modified  to ''

--- src/models/query.js
+++ src/models/query.js
@@ -3,7 +3,7 @@ const query = {
     FROM TodoList.tasks ORDER BY title, created_at, status, description`,
   queryCreateTask: `INSERT INTO TodoList.tasks
     (title, status, description) VALUES (?, ?, ?)`,
-  queryDeleteTask: `DELETE FROM TodoList.tasks WHERE id = ?`,
+  queryDeleteTask: 'DELETE FROM TodoList.tasks WHERE id = ?',
 };
 
 module.exports = query;
