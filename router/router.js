e98a51e4c5929866520d7b0e42e7fba5e62383ce
Oieusouopi <rafasc866@gmail.com>
2022-07-04 17:50:04 -0300

    add: router to edit tasks

--- router/router.js
+++ router/router.js
@@ -7,4 +7,10 @@ router.post('/task', taskListController.createTask);
 
 router.delete('/task/:id', taskListController.deleteTask);
 
+router.put('/taskTitle/:id', taskListController.editTaskTitle);
+
+router.put('/taskDesc/:id', taskListController.editTaskDescription);
+
+router.put('/taskStatus/:id', taskListController.editTaskStatus);
+
 module.exports = router;
