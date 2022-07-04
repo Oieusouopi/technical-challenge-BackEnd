d79e8cbbedcf32960bdea6321d0904f6f214be7d
Oieusouopi <rafasc866@gmail.com>
2022-07-04 17:59:37 -0300

    rename: rename validMessagesMock.js

---
+++ tests/unit/helper/validMessagesMock/validMessagesMock.js
@@ -0,0 +1,8 @@
+const validMessagesMock = {
+  TITLEMOCKSIZE: 'The length of the "title" is incorrect must be less than 30 characters',
+  STATUSERRORMOCK: 'The "status" is wrong must be Pendente, Error, Sucesso or Em andamento',
+  DESCRIPTIONERRORMOCK: `The length of the "description" is incorrect must be less than 500 characters`,
+  RESPONSIBLEUSERINVALI: 'The length of the "responsible_user" is incorrect must be less than 30 characters',
+};
+
+module.exports = validMessagesMock;
