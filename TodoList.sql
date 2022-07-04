b94d4090907eff8a01ae5c9b1847f787db8ea816
Oieusouopi <rafasc866@gmail.com>
2022-07-04 17:49:33 -0300

    add: add responsibleUser in todolist.sql

--- TodoList.sql
+++ TodoList.sql
@@ -6,15 +6,16 @@ USE TodoList;
 
 CREATE TABLE tasks (
     id INT NOT NULL auto_increment,
-    title varchar(30) NOT NULL,
+    responsible_user VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     status VARCHAR(30) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP(),
     description VARCHAR(500) NOT NULL,
     PRIMARY KEY(id)
 );
 
-INSERT INTO TodoList.tasks (title, status, description) VALUES
-    ('manutenção','Pendente', 'realizar manutenção na maquina'),
-    ('limpeza','Em Andamaneto', 'desentupir os canos'),
-    ('atendimento','Sucesso', 'atender os clientes de empresa x'),
-    ('atendimento', 'Sucesso', 'organizar as mesas');
+INSERT INTO TodoList.tasks (title,responsible_user, status, description) VALUES
+    ('manutenção', 'Rafael Carvalho','Pendente', 'realizar manutenção na maquina'),
+    ('limpeza', 'Angela Santos','Em Andamaneto', 'desentupir os canos'),
+    ('atendimento', 'Larissa Flora','Sucesso', 'atender os clientes de empresa x'),
+    ('atendimento', 'Helen Rodrigues','Sucesso', 'organizar as mesas');
