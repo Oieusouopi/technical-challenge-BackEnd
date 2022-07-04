918572c7ed76fdb1d01d985412a5f8f3e38eabf0
Oieusouopi <rafasc866@gmail.com>
2022-07-04 17:57:38 -0300

    tests: add test to edit tasks

--- tests/unit/controllers/taskListControllers.spec.js
+++ tests/unit/controllers/taskListControllers.spec.js
@@ -2,10 +2,13 @@ const {describe, it} = require('mocha');
 const {expect} = require('chai');
 const sinon = require('sinon');
 
+// HELPERS
+const allTaskList = require('../helper/allTaskMock');
+const newTaskMock = require('../helper/newTaskMock');
+
 const taskListServices = require('../../../src/services/taskListServices');
 const taskListControllers = require(
     '../../../src/controllers/taskListControllers');
-const allTaskList = require('../helper/allTaskMock');
 
 describe('1 - taskListControllers.getAllTaskList() wihout error', () => {
   const req = {};
@@ -44,9 +47,9 @@ describe('2 - taskListControllers.createTask() wihout error', () => {
   beforeEach(() => {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
-    // next = sinon.stub().returns();
     req.body = {
       title: 'atendimento',
+      responsibleUser: 'algum usuario',
       status: 'pendente',
       description: 'limpeza de papeis que não utilizaremos',
     };
@@ -88,5 +91,83 @@ describe('3 - taskListControllers.deleteTask() wihout error', () => {
   returng a res status 204`, async () => {
     await taskListControllers.deleteTask(req, res, next);
     expect(res.status.calledWith(204)).to.be.true;
+  });
+});
+
+describe('4 - taskListControllers.editTaskTitle() wihout error', () => {
+  const req = {};
+  const res = {};
+  const next = () => {};
+
+  beforeEach(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    req.params = {id: 1};
+    req.body = {title: newTaskMock.newTitle};
+
+    sinon.stub(taskListServices, 'editTaskTitle')
+        .resolves('Title edited with sucess');
+  });
+
+  afterEach(() => {
+    taskListServices.editTaskTitle.restore();
+  });
+
+  it(`1 - Function to edit title task in the controller
+  return a res status 200`, async () => {
+    await taskListControllers.editTaskTitle(req, res, next);
+    expect(res.status.calledWith(200)).to.be.true;
+  });
+});
+
+describe('5 - taskListControllers.editTaskDescription() wihout error', () => {
+  const req = {};
+  const res = {};
+  const next = () => {};
+
+  beforeEach(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    req.params = {id: 1};
+    req.body = {description: newTaskMock.newDescription};
+
+    sinon.stub(taskListServices, 'editTaskDescription')
+        .resolves('Description edited with sucess');
+  });
+
+  afterEach(() => {
+    taskListServices.editTaskDescription.restore();
+  });
+
+  it(`1 - Function to edit description task in the controller
+  return a res status 200`, async () => {
+    await taskListControllers.editTaskDescription(req, res, next);
+    expect(res.status.calledWith(200)).to.be.true;
+  });
+});
+
+describe('6 - taskListControllers.editTaskStatus() wihout error', () => {
+  const req = {};
+  const res = {};
+  const next = () => {};
+
+  beforeEach(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    req.params = {id: 1};
+    req.body = {status: newTaskMock.newStatus};
+
+    sinon.stub(taskListServices, 'editTaskStatus')
+        .resolves('Status edited with sucess');
+  });
+
+  afterEach(() => {
+    taskListServices.editTaskStatus.restore();
+  });
+
+  it(`1 - Function to edit status task in the controller
+  return a res status 200`, async () => {
+    await taskListControllers.editTaskStatus(req, res, next);
+    expect(res.status.calledWith(200)).to.be.true;
   });
 });
